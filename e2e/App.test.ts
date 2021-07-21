1e8fe0f1e1c8d9c2d4c8d9e4629bd46f9a24fac1
Avery <averybrowndev@gmail.com>
2021-07-21 16:12:26 -0400

    demo screenshot bug

--- e2e/App.test.ts
+++ e2e/App.test.ts
@@ -1,14 +1,22 @@
 import { test } from './baseFixtures';
 
+
+test.use({
+  headless: false,
+    viewport: { width: 1440, height: 800 },
+    screenshot: 'only-on-failure', //Only creates empty folder
+})
+
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:3000")
 })
 
 test('use Turquoise as a default background color', async ({ page }) => {
-  await page.waitForSelector("text=#1abc9c")
+  await page.waitForSelector("text=#ffffff") //changed so it will fail, triggers folder creation
 });
 
 test('use Red as a background color', async ({ page }) => {
   await page.click("text=Red")
   await page.waitForSelector("text=#e74c3c")
+  //await page.screenshot({ path: 'screenshot.png', fullPage: true }); THIS DOES WORK
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,13 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+
+  outputDir: './e2e_screenshots',
+  use: {
+    headless: false,
+    viewport: { width: 1440, height: 800 },
+    screenshot: 'only-on-failure', //Only creates empty folder
+  },
   projects: [
     {
       name: 'Chrome',
